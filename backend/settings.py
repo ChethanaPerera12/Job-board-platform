973e1d4b71d3244c761f6b18a0661d3dfad66eab
Rasanjali-Perera <chethanaperera0912@gmail.com>
2026-08-03 11:15:44 +0530

    Add users and jobs applications with initial setup and models

--- backend/settings.py
+++ backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'users.User'
