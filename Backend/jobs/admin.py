e0ed8238bfd28d3a168a4ec83fb31b5597b4a334
Chethana Perera <chethanaperera0912@gmail.com>
2026-08-03 15:12:34 +0530

    Delete Backend directory

--- Backend/jobs/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Company, Category, Job, Application
-
-admin.site.register(Company)
-admin.site.register(Category)
-admin.site.register(Job)
-admin.site.register(Application)
